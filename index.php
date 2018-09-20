7aa1c87651accacdb9013adc93be6e3b799656ad
Zosia <owsiak@student.agh.edu.pl>
2018-09-20 15:07:36 +0200

    Changed admin welcome message

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?/*php 
-session_start(); 
+<?/*php
+session_start();
 
 if (isset($_SESSION['id'])==1) {
   header('location: login.php');
@@ -29,7 +29,7 @@ if (isset($_GET['logout'])) {
 
 <div class="row">
 <div class="col-sm-8 float-none tab-content" >
- 
+
 
 
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
@@ -38,25 +38,25 @@ if (isset($_GET['logout'])) {
         <img class="d-block w-100" src="images/karuzela0.jpg" alt="First slide">
         <div class="carousel-caption d-none d-md-block">
          <h2>Dzień dobry<?php  if (isset($_SESSION['username'])){echo ", ", $_SESSION['username'];} ?>!</h2>
-      
+
       <?php  if (isset($_SESSION['username']) && $_SESSION['username']=="koala") : ?>
-        <h3>adminie! </h3>
+        <h3>Jesteś adminem na tym dancingu. </h3>
       <?php endif ?>
       <p>Witaj w magicznym świecie liści!</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="images/karuzela1.jpg" alt="Second slide"> 
+        <img class="d-block w-100" src="images/karuzela1.jpg" alt="Second slide">
         <div class="carousel-caption d-none d-md-block">
           <h3>Sprawdź rozpoznawacz liści</h3>
-        
+
         </div>
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="images/karuzela2.jpg" alt="Third slide"> 
+        <img class="d-block w-100" src="images/karuzela2.jpg" alt="Third slide">
         <div class="carousel-caption d-none d-md-block">
           <h3>Lub już dziś poznaj nowy liść!</h3>
-          
+
         </div>
       </div>
     </div>
@@ -73,7 +73,7 @@ if (isset($_GET['logout'])) {
   <div class="col-sm-4 float-none" style = "padding: 1em">
     <div class="alert alert-primary tab-content" role="alert">
       <h2>Kącik wróżb </h2>
-      <p>Wybierz swoje drzewo i zobacz, co czeka Cię w roku 2018! </p> 
+      <p>Wybierz swoje drzewo i zobacz, co czeka Cię w roku 2018! </p>
 
 <img src="images/modrzew.jpg" id="modrzew" class="drzewa" onclick="diffImage(this)" alt="drzewo">
 <img src="images/klonklon.jpg" id="klonklon" class="drzewa" onclick="diffImage(this)" alt="drzewo">
@@ -106,5 +106,3 @@ if (isset($_GET['logout'])) {
 
 </body>
 </html>
-
-
